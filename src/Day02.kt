ed8e940d2da18c1b21baad1c4280c2ab8544f70b
Mike Macaulay <michael.macaulay@aviture.us.com>
2023-12-14 22:35:51 -0600

    day2 part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,5 +1,3 @@
-import kotlin.math.log
-
 enum class Color() {
     RED,
     BLUE,
@@ -17,18 +15,23 @@ fun main(){
     data class Pull(val number: Int, val color: Color)
     data class Game(val id: Int, val pulls: List<Pull>)
 
-    fun part1(input: List<String>): Int {
-        val games = input.map{game ->
+    fun parseGames(input: List<String>): List<Game> {
+        val games = input.map { game ->
             val (gameStr, allRolls) = game.split(':')
             val (_, id) = gameStr.split(" ")
-            val pulls = allRolls.trim().split(";").flatMap {pulls ->
-                pulls.split(",").map{
+            val pulls = allRolls.trim().split(";").flatMap { pulls ->
+                pulls.split(",").map {
                     val (num, color) = it.trim().split(" ")
                     Pull(num.toInt(), txtToColor[color]!!)
                 }
             }
             Game(id.toInt(), pulls)
         }
+        return games
+    }
+
+    fun part1(input: List<String>): Int {
+        val games = parseGames(input)
 
         return games.sumOf {game ->
             if(
@@ -43,6 +46,18 @@ fun main(){
         }
     }
 
+    fun part2(input: List<String>): Int {
+        val games = parseGames(input)
+        return games.sumOf {game ->
+            val maxRed = game.pulls.filter { it.color == Color.RED }.maxOf { it.number }
+            val maxBlue = game.pulls.filter { it.color == Color.BLUE }.maxOf { it.number }
+            val maxGreen = game.pulls.filter { it.color == Color.GREEN }.maxOf { it.number }
+            maxRed * maxBlue * maxGreen;
+        }
+    }
+
     val input = readInput("Day02")
-    println(part1(input))
+    val testInput = readInput("Day02_test")
+//    println(part1(input))
+    println(part2(input))
 }
